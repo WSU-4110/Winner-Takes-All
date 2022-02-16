b158a415e6f95fb7cc47dbd3757d06cbdcb2dc4a
Donovan Galloway <dvan0822@gmail.com>
2022-02-15 21:51:22 -0500

    Button Functionalities & New Activity Pages

--- app/src/main/java/com/example/winner_takes_all/MainActivity.java
+++ app/src/main/java/com/example/winner_takes_all/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.winner_takes_all;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,68 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+
+
+        openNewActivity();
+        toast.show();
+
+    }
+
+    public void showToastserv(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+
+
+        openserv();
+        toast.show();
+
+    }
+
+    public void showToastrank(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+
+
+        openrank();
+        toast.show();
+
+    }
+
+    public void showToastleads(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+
+
+        openleads();
+        toast.show();
+
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+
+    }
+
+    public void openserv(){
+        Intent intent = new Intent(this, servers.class);
+        startActivity(intent);
+
+    }
+
+    public void openrank(){
+        Intent intent = new Intent(this, Rankings.class);
+        startActivity(intent);
+
+    }
+
+    public void openleads(){
+        Intent intent = new Intent(this, Leaderboards.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/winner_takes_all/Rankings.java
@@ -0,0 +1,14 @@
+package com.example.winner_takes_all;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rankings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rankings);
+    }
+}
