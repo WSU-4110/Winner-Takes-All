3332ef6436b72f238ba9353ac8d54cec09d3bb2e
Akshat Sikligar <gv7119@wayne.edu>
2022-02-21 16:56:24 -0500

    DarkMode setting (Akshat S. gv7119)

--- app/src/main/java/com/example/winner_takes_all/Settings.java
+++ app/src/main/java/com/example/winner_takes_all/Settings.java
@@ -1,15 +1,18 @@
 package com.example.winner_takes_all;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class Settings extends AppCompatActivity {
 
     private Button button;
+    private RadioGroup rG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,21 @@ public class Settings extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 opensetuser();
+            }
+        });
+
+        rG = findViewById(R.id.Butongroup);
+        rG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.Light:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                    case R.id.Dark:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                }
             }
         });
     }
