cb18202dc05c3ecce936062128ded06fa01a73ef
Donovan Galloway <dvan0822@gmail.com>
2022-02-22 14:28:16 -0500

    Opening Page

---
+++ app/src/main/java/com/example/winner_takes_all/openingscreen.java
@@ -0,0 +1,49 @@
+package com.example.winner_takes_all;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class openingscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_openingscreen);
+    }
+
+    public void signin_open(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+
+
+        opensignin();
+        toast.show();
+
+    }
+
+    public void signup_open(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+
+
+        opensignup();
+        toast.show();
+
+    }
+
+    public void opensignup(){
+        Intent intent = new Intent(this, register.class);
+        startActivity(intent);
+
+    }
+
+    public void opensignin(){
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
+
+    }
+}
