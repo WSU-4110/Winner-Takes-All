6a3c91cda8d980ef39361cf4077c2634dcae60e0
Jeremiah Grant <gj8206@wayne.edu>
2022-03-12 16:31:39 -0500

    Ok brotha! I made some decent progress on the leaderboard finally. So leaderboard page is basically what we are currently using to increase the score. Since there isnt currently a way to increase score I made a button, "+1" this will add scores.      The 2nd button, End will stop that page and open LeaderboardsHistory where it correctly displays all currently stored Scores. Your last score and the 3 top scores.      I haven't connected to firebase yet. & obviously score has no real implementation yet.      The code I'm gonna push though is easily manageable and modifiable. We could easily set up a for loop for the score history to go on forever.      All we really need is to connect to Firestore for the Username integration. And if we go this route, pulling score from firestore. Though think Mohsun wanted to do score app-side (which will be the way to go imo). Also to start thinking about how Score is going to be added to each player.  Finally, cleaning up how the page look.

--- app/src/main/java/com/example/winner_takes_all/Leaderboards.java
+++ app/src/main/java/com/example/winner_takes_all/Leaderboards.java
@@ -1,28 +1,19 @@
 package com.example.winner_takes_all;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.auth.User;
 
 public class Leaderboards extends AppCompatActivity {
     FirebaseAuth fAuth;
@@ -72,12 +63,24 @@ public class Leaderboards extends AppCompatActivity {
         endScore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("lastScore", score);
+                editor.apply();
 
+                Intent intent = new Intent(getApplicationContext(), LeaderboardsHistory.class);
+                startActivity(intent);
+                finish();
             }
         });
+
+
     }
 
+
  /*   @Override protected void onStart() {
+         // Best activity
+        setContentView(R.layout.activity_leaderboard_history);
         super.onStart();
         FirebaseFirestore firestore = FirebaseFirestore.getInstance();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
