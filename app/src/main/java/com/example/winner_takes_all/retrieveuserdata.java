771f14497a0b6fad510ddc88b344fafddbf606b6
Akshat Sikligar <gv7119@wayne.edu>
2022-03-27 15:38:19 -0400

    User Profile and change User Name setup (Akshat S. gv7119)

---
+++ app/src/main/java/com/example/winner_takes_all/retrieveuserdata.java
@@ -0,0 +1,45 @@
+package com.example.winner_takes_all;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+
+
+public class retrieveuserdata extends AppCompatActivity {
+    TextView Email, UserName;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieveuserdata);
+        UserName = findViewById(R.id.Name);
+        Email = findViewById(R.id.Email2);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                UserName.setText(value.getString("UserName"));
+                Email.setText(value.getString("Email"));
+            }
+        });
+
+    }
+}
