3b69fc1a1d460de99cd5c6734aeb9aaf18d6e49e
Jegra22 <gj8206@wayne.edu>
2022-03-30 20:57:15 -0400

    Username working, general code cleanup and UI improvements. Thanks Mohsun!

--- app/src/main/java/com/example/winner_takes_all/Leaderboards.java
+++ app/src/main/java/com/example/winner_takes_all/Leaderboards.java
@@ -1,71 +1,56 @@
 package com.example.winner_takes_all;
-import androidx.annotation.NonNull;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.storage.FirebaseStorage;
+
+
 
 public class Leaderboards extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    String userId;
-
-    private RecyclerView recyclerView;
-    peopleAdapter adapter;
-    DatabaseReference database;
-    ImageView imageView;
-    TextView UserName, Score;
+    String userID;
+    TextView UserName, Score, UserI; // check this
     Button addScore, endScore;
     int score = 0;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboards);
-        UserName = findViewById(R.id.UserName);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+
+        UserI = findViewById(R.id.useRID);
+        userID = fAuth.getCurrentUser().getUid();
+        UserName = findViewById(R.id.Name);
         Score = findViewById(R.id.Score);
         addScore = (Button) findViewById(R.id.addScore);
         endScore = (Button) findViewById(R.id.endScore);
 
-        fAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
-        database = FirebaseDatabase.getInstance().getReference();
-
-        userId = fAuth.getCurrentUser().getUid();
-        DocumentReference documentReference = fStore.collection("users").document(userId);
+        DocumentReference documentReference = fStore.collection("users").document(userID);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException error) {
-
-                UserName.setText(documentSnapshot.getString("UserName: " + UserName)); /* + User */
-                Score.setText(documentSnapshot.getString("Score: " + score));
-                /* Buttons for add score & end score. Change these to score values later */
-                UserName.setText("UserName: " + UserName );
-                Score.setText("Score: " + score );
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                UserName.setText(value.getString("UserName:"));
+                Score.setText("Score: " + score);
+/* value.getString( */
                 addScore.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View view) {
                         score++;
-                        UserName.setText("UserName: " + UserName);
-                        Score.setText("Score: " +score );
+                        UserName.setText(value.getString("UserName:"));
+                        Score.setText("Score: " + score);
                     }
                 });
 
--- app/src/main/java/com/example/winner_takes_all/LeaderboardsHistory.java
+++ app/src/main/java/com/example/winner_takes_all/LeaderboardsHistory.java
@@ -1,12 +1,12 @@
+
 package com.example.winner_takes_all;
 
-import android.annotation.SuppressLint;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,37 +14,52 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import java.util.Arrays;
 
 public class LeaderboardsHistory extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    String userId;
+    String userID;
 
     private RecyclerView recyclerView;
     DatabaseReference database;
-    TextView UserName, Score, YourScore;
+    TextView UserName, Score, YourScore, UserI;
     int lastScore;
     int best1, best2, best3;
     int score = lastScore;
 
-
-    protected void onCreate(Bundle savedInstanceState){
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_history);
-        UserName = findViewById(R.id.UserName);
-        Score = findViewById(R.id.Score);
-        YourScore = findViewById(R.id.YourScore);
-
-
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
+
+        UserI = findViewById(R.id.useRID);
+        userID = fAuth.getCurrentUser().getUid();
+        UserName = findViewById(R.id.Name);
+        Score = findViewById(R.id.Score);
+        YourScore = findViewById(R.id.YourScore);
         database = FirebaseDatabase.getInstance().getReference();
+
+
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                UserName.setText(value.getString("UserName:"));
+                Score.setText(value.getString("Score" + score));
+            }
+        });
+
         // load previous score history
         SharedPreferences preferences = getSharedPreferences("PREFS", 0);
         lastScore = preferences.getInt("lastScore", 0); /* the score you just got */
@@ -52,53 +67,51 @@ public class LeaderboardsHistory extends AppCompatActivity {
         best2 = preferences.getInt("best2", 0);
         best3 = preferences.getInt("best3", 0);
 
-        userId = fAuth.getCurrentUser().getUid();
-        DocumentReference documentReference = fStore.collection("users").document(userId);
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @SuppressLint("SetTextI18n")
-            public void onEvent (DocumentSnapshot documentSnapshot, FirebaseFirestoreException error){
-                UserName.setText(documentSnapshot.getString("UserName" + userId));
-                Score.setText(documentSnapshot.getString("Score" + score));
-          /*      best1.orderBy(Score).limit(3); */
+                   /*
+               // query for firestore
+               CollectionReference scoreRef = (CollectionReference) fStore.collection("users")
+                       .whereGreaterThanOrEqualTo("Score", Arrays.asList(0)).orderBy("UserName")
+                       .orderBy(String.valueOf(Score), Query.Direction.DESCENDING).limit(50);
+               */
 
 
-                // replace scores if new highscore
-                if(lastScore > best3){
-                    best3 = lastScore;
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.putInt("best3", best3);
-                    editor.apply();
-                }
-                if(lastScore > best2){
-                    int temp = best2;
-                    best2 = lastScore;
-                    best3 = temp;
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.putInt("best3", best3);
-                    editor.putInt("best2", best2);
-                    editor.apply();
-                }
-                if(lastScore > best1){
-                    int temp = best1;
-                    best1 = lastScore;
-                    best2 = temp;
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.putInt("best2", best2);
-                    editor.putInt("best1", best1);
-                    editor.apply();
-                }
-                // displays your userid and score
-                YourScore.setText("UserName: " + userId + ":" + "\n" +
-                        " Your Score: " + lastScore + "\n"
-                );
-                // displays top scores
-                Score.setText("#1 on app: " + best1 + "\n" +
-                        "#2 on app: " + best2 + "\n" +
-                        "#3 on app: " + best3);
+        // replace scores if new highscore
+        if (lastScore > best3) {
+            best3 = lastScore;
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("best3", best3);
+            editor.apply();
+        }
+        if (lastScore > best2) {
+            int temp = best2;
+            best2 = lastScore;
+            best3 = temp;
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("best3", best3);
+            editor.putInt("best2", best2);
+            editor.apply();
+        }
+        if (lastScore > best1) {
+            int temp = best1;
+            best1 = lastScore;
+            best2 = temp;
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("best2", best2);
+            editor.putInt("best1", best1);
+            editor.apply();
+        }
+        // displays your userid and score
+        YourScore.setText("UserName: " + UserName + ":" + "\n" +
+                " Your Score: " + lastScore + "\n"
+        );
+        // displays top scores
+        Score.setText("#1 on app: " + best1 + "\n" +
+                "#2 on app: " + best2 + "\n" +
+                "#3 on app: " + best3
 
-            }
-        });
+        );
     }
+
     @Override
     public void onBackPressed(){
         Intent intent = new Intent(getApplicationContext(), Leaderboards.class);
