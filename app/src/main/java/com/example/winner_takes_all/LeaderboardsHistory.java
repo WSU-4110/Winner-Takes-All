a0f9c7dc89cb156b14c0260301bb28f61e8a8df5
Jegra22 <gj8206@wayne.edu>
2022-03-30 22:03:23 -0400

    Ui updated. Starting to work on leaderboard history. will need to do an array to gather top user data. also need a scroll view.

--- app/src/main/java/com/example/winner_takes_all/LeaderboardsHistory.java
+++ app/src/main/java/com/example/winner_takes_all/LeaderboardsHistory.java
@@ -5,16 +5,12 @@ package com.example.winner_takes_all;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -29,10 +25,7 @@ public class LeaderboardsHistory extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userID;
-
-    private RecyclerView recyclerView;
-    DatabaseReference database;
-    TextView UserName, Score, YourScore, UserI;
+    TextView UserName, Score, YourScore, UserI, TheirScore;
     int lastScore;
     int best1, best2, best3;
     int score = lastScore;
@@ -48,68 +41,28 @@ public class LeaderboardsHistory extends AppCompatActivity {
         UserName = findViewById(R.id.Name);
         Score = findViewById(R.id.Score);
         YourScore = findViewById(R.id.YourScore);
-        database = FirebaseDatabase.getInstance().getReference();
-
+        TheirScore = findViewById(R.id.TheirScore);
 
         DocumentReference documentReference = fStore.collection("users").document(userID);
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+                lastScore = preferences.getInt("lastScore", 0);
+                best1 = preferences.getInt("best1", 0);
+                best2 = preferences.getInt("best2", 0);
+                best3 = preferences.getInt("best3", 0);
+
+                /* UserName.setText(value.getString("UserName:"));
+                Score.setText("Score: " + score);
                 UserName.setText(value.getString("UserName:"));
-                Score.setText(value.getString("Score" + score));
+                Score.setText("Score: " + score); */
+
+                YourScore.setText(value.getString("UserName:") + "'s " + ("Score: " + lastScore));
+                /* TheirScore.setText(value.getString("UserName:")+ "'s " + ("Score: " + score)); */
+
             }
         });
-
-        // load previous score history
-        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
-        lastScore = preferences.getInt("lastScore", 0); /* the score you just got */
-        best1 = preferences.getInt("best1", 0);
-        best2 = preferences.getInt("best2", 0);
-        best3 = preferences.getInt("best3", 0);
-
-                   /*
-               // query for firestore
-               CollectionReference scoreRef = (CollectionReference) fStore.collection("users")
-                       .whereGreaterThanOrEqualTo("Score", Arrays.asList(0)).orderBy("UserName")
-                       .orderBy(String.valueOf(Score), Query.Direction.DESCENDING).limit(50);
-               */
-
-
-        // replace scores if new highscore
-        if (lastScore > best3) {
-            best3 = lastScore;
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("best3", best3);
-            editor.apply();
-        }
-        if (lastScore > best2) {
-            int temp = best2;
-            best2 = lastScore;
-            best3 = temp;
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("best3", best3);
-            editor.putInt("best2", best2);
-            editor.apply();
-        }
-        if (lastScore > best1) {
-            int temp = best1;
-            best1 = lastScore;
-            best2 = temp;
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putInt("best2", best2);
-            editor.putInt("best1", best1);
-            editor.apply();
-        }
-        // displays your userid and score
-        YourScore.setText("UserName: " + UserName + ":" + "\n" +
-                " Your Score: " + lastScore + "\n"
-        );
-        // displays top scores
-        Score.setText("#1 on app: " + best1 + "\n" +
-                "#2 on app: " + best2 + "\n" +
-                "#3 on app: " + best3
-
-        );
     }
 
     @Override
