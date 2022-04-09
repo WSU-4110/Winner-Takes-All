1179045aaf3bb5e3c722b444ac43b53cfe88a802
Akshat Sikligar <gv7119@wayne.edu>
2022-04-08 21:41:15 -0400

    Homework4 (Akshat S. gv7119)

--- app/src/main/java/com/example/winner_takes_all/ChangePassword.java
+++ app/src/main/java/com/example/winner_takes_all/ChangePassword.java
@@ -28,6 +28,7 @@ public class ChangePassword extends AppCompatActivity {
     public ProgressDialog login;
     ProgressDialog load;
 
+    //main
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,63 +45,73 @@ public class ChangePassword extends AppCompatActivity {
         Cpass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PasswordResetDialog();
+                //resetPassword;
             }
         });
 
     }
 
 
-    private void PasswordResetDialog() {
-        AlertDialog.Builder builder=new AlertDialog.Builder(this);
-        builder.setTitle("Enter Email");
-        LinearLayout linearLayout=new LinearLayout(this);
-        final EditText emailtext= new EditText(this);
-        emailtext.setText("");
-        emailtext.setMinEms(16);
-        emailtext.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
-        linearLayout.addView(emailtext);
-        linearLayout.setPadding(15,15,15,15);
-        builder.setView(linearLayout);
-        builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String emaill=emailtext.getText().toString().trim();
-                ChangePass(emaill);
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        builder.create().show();
-    }
 
-    private void ChangePass(String emailUser) {
-        load=new ProgressDialog(this);
-        load.setMessage("Sending Reset Email");
-        load.setCanceledOnTouchOutside(false);
-        load.show();
-        mAuth.sendPasswordResetEmail(emailUser).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                load.dismiss();
-                if(task.isSuccessful())
-                {
-                    Toast.makeText(ChangePassword.this,"Sent Email",Toast.LENGTH_LONG).show();
+
+    //new class to reset the password and send dialog
+    class resetPassword{
+        private void PasswordResetDialog() {
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setTitle("Enter Email");
+            LinearLayout linearLayout=new LinearLayout(this);
+            final EditText emailtext= new EditText(this);
+            emailtext.setText("");
+            emailtext.setMinEms(16);
+            emailtext.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+            linearLayout.addView(emailtext);
+            linearLayout.setPadding(15,15,15,15);
+            builder.setView(linearLayout);
+            builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String emaill=emailtext.getText().toString().trim();
+                    ChangePass(emaill);
                 }
-                else {
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.create().show();
+        }
+    }
+
+
+    //new class to change the password to a new one
+    class changepass{
+        private void ChangePass(String emailUser) {
+            //load=new ProgressDialog(this);
+            load.setMessage("Sending Reset Email");
+            load.setCanceledOnTouchOutside(false);
+            load.show();
+            mAuth.sendPasswordResetEmail(emailUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    load.dismiss();
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(ChangePassword.this,"Sent Email",Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        Toast.makeText(ChangePassword.this,"Error",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception error) {
+                    load.dismiss();
                     Toast.makeText(ChangePassword.this,"Error",Toast.LENGTH_LONG).show();
                 }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception error) {
-                load.dismiss();
-                Toast.makeText(ChangePassword.this,"Error",Toast.LENGTH_LONG).show();
-            }
-        });
+            });
+        }
     }
+
 }
